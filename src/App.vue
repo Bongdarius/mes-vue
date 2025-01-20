c7ee01afe561bffe04f88ca529e7c87918e7043c
Bongdarius <okuo94@gnu.ac.kr>
2025-01-21 08:42:02 +0900

    remove: 필요하지 않은 거 삭제
    remove: 필요하지 않은 거 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-// import { default as HelloWorld } from './components/HelloWorld.vue'; /* PartiallyEnd: #3632/scriptSetup.vue */
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
   <div>
-    <div class="router">
-      <RouterLink to="/">Home</RouterLink> |
-      <RouterLink to="/about">About</RouterLink> |
-      <RouterLink to="/flexGrid">FlexGrid</RouterLink> |
-      <RouterLink to="/trackChanges">변경내역추적</RouterLink> |
-      <RouterLink to="/columnPicker">열선택기</RouterLink> |
-      <RouterLink to="/customerMngPage">거래처관리</RouterLink> |
-      <RouterLink to="/commonFlexGrid">그리드공통</RouterLink>
+    <div class="router flex justify-center text-green-700">
+      <RouterLink to="/">그리드공통</RouterLink>
     </div>
     <RouterView />
   </div>
